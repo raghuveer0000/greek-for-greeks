fbc65c47493b9ec972637ca86b4ec97a3611e779
Narsapur Raghuveer <raghuveernarsapur@gmail.com>
2025-08-19 18:27:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,45 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    int getSecondLargest(int arr[]) {
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MIN_VALUE;
-        
-        for (int num : arr) {
-            if (num > largest) {
-                secondLargest = largest;
-                largest = num;
-            } else if (num > secondLargest && num != largest) {
-                secondLargest = num;
+    public int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        int first = -1, second = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > first) {
+                
+                second = first;
+                first = arr[i];
+            } else if (arr[i] < first && arr[i] > second) {
+                
+                second = arr[i];
             }
         }
-        
-        return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+
+        return second;
     }
 }
-
